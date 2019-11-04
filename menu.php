2aa1d505be2238ba7d8ea48b25e344cff8a39ab5
Leonardo Rodrigues <leo.intis@gmail.com>
2019-11-04 13:13:57 -0300

    Criação págs base

---
+++ menu.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-expand-md bg-primary navbar-dark">
+  <!-- Brand -->
+  <a class="navbar-brand" href="inicio.php">Home</a>
+
+  <!-- Toggler/collapsibe Button -->
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <!-- Navbar links -->
+  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="lancamentos.php">Lançamentos</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="relatorios.php">Relatórios</a>
+      </li>
+    </ul>
+  </div>
+</nav>
